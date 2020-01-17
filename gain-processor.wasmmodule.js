12eae2c9d6eca961a504ece166d460847adf3c38
Thomas Deppisch <thomas.deppisch@student.kug.ac.at>
2020-01-17 18:41:51 +0100

    removed export statements in wasm-audio-helper and gain-processor.wasmmodule -> attach these to wasm-worklet-processor directly without import

--- gain-processor.wasmmodule.js
+++ gain-processor.wasmmodule.js
@@ -20,7 +20,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-
+// {{PRE_JSES}}
 
 // Sometimes an existing Module object exists with properties
 // meant to overwrite the default module functionality. Here
@@ -3798,25 +3798,4 @@ run();
 // {{MODULE_ADDITIONS}}
 
 
-
-/**
- * Copyright 2018 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
-// EXPORT_ES6 option does not work as described at
-// https://github.com/kripken/emscripten/issues/6284, so we have to
-// manually add this by '--post-js' setting when the Emscripten compilation.
-export default Module;
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,20 @@ module.exports = {
   output: {
     filename: 'bundle.js'
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.m?js$/,
-  //       exclude: /(node_modules|bower_components)/,
-  //       use: {
-  //         loader: 'babel-loader',
-  //         options: {
-  //           presets: ['@babel/preset-env']
-  //         }
-  //       }
-  //     }
-  //   ]
-  // },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   resolve: {
     extensions: ['*', '.js']
   },
