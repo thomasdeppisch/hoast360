7fbb8cd58f9d4d807b7608cf9cd202cb4dd6af8c
Thomas Deppisch <thomas.deppisch@sonible.com>
2020-04-16 19:34:40 +0200

    added live scripts

---
+++ scripts/live/create_video_mpd.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ffmpeg -re -f webm_dash_manifest -live 1 -i './media/video_360.hdr' -c copy -map 0 \
+-f webm_dash_manifest -live 1 -adaptation_sets "id=0,streams=0" \
+-chunk_start_index 1 -chunk_duration_ms 2000 -time_shift_buffer_depth 7200 -minimum_update_period 7200 './media/video.mpd'
