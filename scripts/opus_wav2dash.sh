d81a5841294cad3aa9e57ea8b16f81ef535dae8b
Thomas Deppisch <thomas.deppisch@student.kug.ac.at>
2020-05-15 22:13:47 +0200

    changed encoding commands to follow google recommendations

--- scripts/opus_wav2dash.sh
+++ scripts/opus_wav2dash.sh
@@ -9,6 +9,9 @@ read ambiorder
 echo 'enter name of source video file with file extension:'
 read vidfn
 
+echo 'enter larger dimension of video resolution, e.g. for 1440x720px enter 1440'
+read vidres
+
 outfn=$infn'_o'$ambiorder
 
 numchannels=$(((ambiorder + 1) * (ambiorder + 1)))
@@ -37,12 +40,15 @@ ffmpeg -i '../videos/orig_'$vidfn -c:v copy -an '../videos/'$vidfn
 ffmpeg -ss 00:00:02 -i '../videos/'$vidfn -vframes 1 -s 360x270 -q:v 5 $outdir'/thumbnail.jpg'
 
 ############################# create scaled videos #############################
-ffmpeg -i '../videos/'$vidfn -r 25 -c:v libvpx-vp9 -s 360x180 -b:v 500k -keyint_min 150 -g 150 -speed 1 -tile-columns 4 -frame-parallel 1 -an -f webm -dash 1 $outdir'/'$outfn'_360_500k.webm'
-ffmpeg -i '../videos/'$vidfn -r 25 -c:v libvpx-vp9 -s 720x360 -b:v 1000k -keyint_min 150 -g 150 -speed 1 -tile-columns 4 -frame-parallel 1 -an -f webm -dash 1 $outdir'/'$outfn'_720_1000k.webm'
-ffmpeg -i '../videos/'$vidfn -r 25 -c:v libvpx-vp9 -s 1080x540 -b:v 3000k -keyint_min 150 -g 150 -speed 1 -tile-columns 4 -frame-parallel 1 -an -f webm -dash 1 $outdir'/'$outfn'_1080_3000k.webm'
-ffmpeg -i '../videos/'$vidfn -r 25 -c:v libvpx-vp9 -s 1440x720 -b:v 4000k -keyint_min 150 -g 150 -speed 1 -tile-columns 4 -frame-parallel 1 -an -f webm -dash 1 $outdir'/'$outfn'_1440_4000k.webm'
-ffmpeg -i '../videos/'$vidfn -r 25 -c:v libvpx-vp9 -s 2560x1280 -b:v 6000k -keyint_min 150 -g 150 -speed 1 -tile-columns 4 -frame-parallel 1 -an -f webm -dash 1 $outdir'/'$outfn'_2560_6000k.webm'
-#ffmpeg -i '../videos/'$vidfn -r 25 -c:v libvpx-vp9 -s 3840x1920 -b:v 10000k -keyint_min 150 -g 150 -speed 1 -tile-columns 4 -frame-parallel 1 -an -f webm -dash 1 $outdir'/'$outfn'_3840_3000k.webm'
+ffmpeg -i '../videos/'$vidfn -r 25 -c:v libvpx-vp9 -s 720x360 -b:v 750k -minrate 375k -maxrate 1088k -crf 33 -quality good -keyint_min 150 -g 150 -speed 1 -tile-columns 1 -frame-parallel 1 -an -f webm -dash 1 $outdir'/'$outfn'_720.webm'
+ffmpeg -i '../videos/'$vidfn -r 25 -c:v libvpx-vp9 -s 1080x540 -b:v 1024k -minrate 512k -maxrate 1485k -crf 32 -quality good -keyint_min 150 -g 150 -speed 1 -tile-columns 2 -frame-parallel 1 -an -f webm -dash 1 $outdir'/'$outfn'_1080.webm'
+ffmpeg -i '../videos/'$vidfn -r 25 -c:v libvpx-vp9 -s 1440x720 -b:v 1800k -minrate 900k -maxrate 2610k -crf 31 -quality good -keyint_min 150 -g 150 -speed 1 -tile-columns 2 -frame-parallel 1 -an -f webm -dash 1 $outdir'/'$outfn'_1440.webm'
+if (( $vidres > 1440 )); then
+    ffmpeg -i '../videos/'$vidfn -r 25 -c:v libvpx-vp9 -s 2560x1280 -b:v 6000k -minrate 3000k -maxrate 8700k -crf 24 -quality good -keyint_min 150 -g 150 -speed 1 -tile-columns 3 -frame-parallel 1 -an -f webm -dash 1 $outdir'/'$outfn'_2560.webm'
+fi
+if (( $vidres > 2560 )); then
+    ffmpeg -i '../videos/'$vidfn -r 25 -c:v libvpx-vp9 -s 3840x1920 -b:v 12000k -minrate 6000k -maxrate 17400k -crf 15 -quality good -keyint_min 150 -g 150 -speed 1 -tile-columns 3 -frame-parallel 1 -an -f webm -dash 1 $outdir'/'$outfn'_3840.webm'
+fi
 
 ############################# audio, non-live, without chunks #############################
 # chunks created internally!
@@ -51,16 +57,39 @@ ffmpeg \
     -c:a libopus -mapping_family 255 -b:a $audiobitrate'k' -vn -f webm -dash 1 $outdir'/'$outfn'.webm'
 
 ############################# CREATE DASH MANIFESTS #############################
+if (( $vidres <= 1440 )); then
+    ffmpeg \
+        -f webm_dash_manifest -i $outdir'/'$outfn'_720.webm' \
+        -f webm_dash_manifest -i $outdir'/'$outfn'_1080.webm' \
+        -f webm_dash_manifest -i $outdir'/'$outfn'_1440.webm' \
+        -c copy -map 0 -map 1 -map 2 \
+        -f webm_dash_manifest \
+        -adaptation_sets 'id=0,streams=0,1,2' \
+        $outdir'/video.mpd'
 
-ffmpeg \
-    -f webm_dash_manifest -i $outdir'/'$outfn'_360_500k.webm' \
-    -f webm_dash_manifest -i $outdir'/'$outfn'_720_1000k.webm' \
-    -f webm_dash_manifest -i $outdir'/'$outfn'_1080_3000k.webm' \
-    -f webm_dash_manifest -i $outdir'/'$outfn'_1440_4000k.webm' \
-    -f webm_dash_manifest -i $outdir'/'$outfn'_2560_6000k.webm' \
+elif (( $vidres <= 2560 )); then
+    ffmpeg \
+    -f webm_dash_manifest -i $outdir'/'$outfn'_720.webm' \
+    -f webm_dash_manifest -i $outdir'/'$outfn'_1080.webm' \
+    -f webm_dash_manifest -i $outdir'/'$outfn'_1440.webm' \
+    -f webm_dash_manifest -i $outdir'/'$outfn'_2560.webm' \
+    -c copy -map 0 -map 1 -map 2 -map 3 \
+    -f webm_dash_manifest \
+    -adaptation_sets 'id=0,streams=0,1,2,3' \
+    $outdir'/video.mpd'
+
+else
+    ffmpeg \
+    -f webm_dash_manifest -i $outdir'/'$outfn'_720.webm' \
+    -f webm_dash_manifest -i $outdir'/'$outfn'_1080.webm' \
+    -f webm_dash_manifest -i $outdir'/'$outfn'_1440.webm' \
+    -f webm_dash_manifest -i $outdir'/'$outfn'_2560.webm' \
+    -f webm_dash_manifest -i $outdir'/'$outfn'_3840.webm' \
     -c copy -map 0 -map 1 -map 2 -map 3 -map 4 \
     -f webm_dash_manifest \
     -adaptation_sets 'id=0,streams=0,1,2,3,4' \
     $outdir'/video.mpd'
+
+fi
 
 ffmpeg -f webm_dash_manifest -i $outdir'/'$outfn'.webm' -c copy -map 0 -f webm_dash_manifest -adaptation_sets 'id=0,streams=0' $outdir'/audio.mpd'
